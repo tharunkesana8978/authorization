dae6aa306ab6b9afd7cb9e37dccbf00b510a3664
babinakkala123 <nakkalababi123@gmail.com>
2024-12-17 16:30:27 +0530

    Integrate Routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
+
 
 
 dotenv.config();
@@ -11,6 +14,7 @@ app.use(express.json());
 
 connectDB();
 
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
