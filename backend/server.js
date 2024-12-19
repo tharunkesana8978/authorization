30edc0499235c53e15bb3f4b6d48f909bdcef910
babinakkala123 <nakkalababi123@gmail.com>
2024-12-19 23:25:15 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 const employeeRoutes = require("./routes/employeeRoutes")
 const authRoutes = require("./routes/authRoutes");
 
@@ -10,7 +10,7 @@ const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 connectDB();
