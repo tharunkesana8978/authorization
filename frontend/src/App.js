763d6f3c6517665b32141ae292faa5e3b766f0d1
tharunkesana8978 <tharunkesana8978@gmail.com>
2024-12-19 17:32:25 +0530

    created frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                
+               
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
