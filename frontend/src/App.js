87ba090acf405818d150446e7a2e6feea688d7fc
tharunkesana8978 <tharunkesana8978@gmail.com>
2024-12-19 18:02:02 +0530

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Login from "./components/Auth/Login";
 
 
 function App() {
     return (
         <Router>
             <Routes>
-                
+                <Route path="/Login" element={<Login />} />
                
             </Routes>
         </Router>
